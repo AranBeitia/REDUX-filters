cf42963f2002bbd099603526254e2d0d4f6da4de
AranBeitia <arantza.beitia@gmail.com>
2022-01-15 20:58:43 +0100

    fetch

--- src/ui/pages/Dashboard.js
+++ src/ui/pages/Dashboard.js
@@ -1,15 +1,26 @@
+import { useEffect, useState } from 'react'
+
+import getData from '../../services/server'
+
 import { Container } from 'react-bootstrap'
 import Header from '../components/Header'
 import CheckboxFeed from '../components/Checkbox/CheckboxFeed'
 import Multirange from '../components/MultiRange/MultiRange'
 
 const Dashboard = () => {
+  const [properties, setProperties] = useState()
+
+  useEffect(() => {
+    const data = getData('http://localhost:3001/properties')
+    setProperties(data)
+  }, [])
   const fields = ['hi', 'ho']
   return (
     <>
       <Header />
       <Container>
         <h1>dashboard</h1>
+        {/* {properties && properties.map((item) => console.log(item))} */}
         <CheckboxFeed fields={fields} label={'Type of home'} />
         <CheckboxFeed fields={fields} label={'Condition'} />
         <CheckboxFeed fields={fields} label={'Bedrooms'} />
