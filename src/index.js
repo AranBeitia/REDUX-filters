e4e92351b8f33f86f8959957545266477bfd57fb
AranBeitia <arantza.beitia@gmail.com>
2022-01-16 12:12:14 +0100

    redux boilerplate

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Provider from './redux/Provider'
 import { BrowserRouter } from 'react-router-dom'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './ui/assets/styles/main.scss'
 import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
---
+++ src/redux/filter/reducer.js
@@ -0,0 +1,7 @@
+import initialState from './state'
+
+const reducer = (state = initialState, action) => {
+  return state
+}
+
+export default reducer
