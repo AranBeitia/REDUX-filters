a263f625297b9222b43173edca15a47dce2beff7
AranBeitia <arantza.beitia@gmail.com>
2022-01-21 12:12:09 +0100

    Style: add library and components

--- src/ui/components/Header.js
+++ src/ui/components/Header.js
@@ -1,19 +1,22 @@
 import { Navbar, Container } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import logo from '../assets/img/logo.png'
 const Header = ({ title }) => {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">
-          <img
-            alt=""
-            src={logo}
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-          />
-          Redux filters
-        </Navbar.Brand>
+        <Link to="/">
+          <Navbar.Brand>
+            <img
+              alt=""
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />
+            <span>Redux filters</span>
+          </Navbar.Brand>
+        </Link>
         <span className="text-light">{title}</span>
       </Container>
     </Navbar>
--- src/ui/pages/Home.js
+++ src/ui/pages/Home.js
@@ -1,12 +1,31 @@
+import { Link } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
+import Button from '@mui/material/Button'
 import Header from '../components/Header'
+import styled from 'styled-components'
+
+export const Wrapper = styled.section`
+  padding: 4rem;
+  background-color: papayawhip;
+`
+
+export const Block = styled.div`
+  display: block;
+  padding: 1rem 0;
+`
 
 const Home = () => {
   return (
     <>
-      <Header />
+      <Header title={'Home'} />
       <Container>
-        <h1>home</h1>
+        <Wrapper>
+          <h1>home</h1>
+          <Button variant="contained">Hello</Button>
+          <Block>
+            <Link to="/dashboard">Dashboard</Link>
+          </Block>
+        </Wrapper>
       </Container>
     </>
   )
